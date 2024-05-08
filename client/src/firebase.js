589da58f86599c29ce3bfdc7aa8f8945209187b3
miantalhaarain <142482493+miantalhaarain@users.noreply.github.com>
2024-05-08 11:05:50 +0500

    Initial commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLNJLs5wQlQaaNzLUsVO0pnhe9UQzmKik",
+  authDomain: "real-estate-app-e5908.firebaseapp.com",
+  projectId: "real-estate-app-e5908",
+  storageBucket: "real-estate-app-e5908.appspot.com",
+  messagingSenderId: "645697509479",
+  appId: "1:645697509479:web:bc5e1125a87a7d537b1cc2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to IDAP Real Estate</h1>
+      <p className='mb-4 text-slate-700'>At IDAP Real Estate, we believe in making the process of buying, selling, and renting properties as seamless and rewarding as possible. Our mission is to empower individuals and families to find their perfect homes or investment opportunities with confidence and ease.</p>
+      <p className='mb-4 text-slate-700'>
+      We envision a world where everyone can find their ideal property, whether it's their dream home, a lucrative investment, or a commercial space to grow their business. Through innovative technology and personalized service, we strive to make real estate transactions transparent, efficient, and enjoyable for all.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team comprises experienced real estate professionals who are passionate about helping our clients achieve their goals. From first-time homebuyers to seasoned investors, we provide expert guidance at every step of the journey.
+Comprehensive Listings: With a vast database of properties, including residential, commercial, and land listings, we offer a diverse range of options to suit every need and budget.</p>
+    </div>
+  )
+}
